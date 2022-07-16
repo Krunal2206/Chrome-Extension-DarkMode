2ae420b7bbfe33d815df8d115c3b5b32c575f28d
Krunal Thakar <72442161+Krunal2206@users.noreply.github.com>
2022-07-16 11:36:39 +0530

    Update background.js

--- background.js
+++ background.js
@@ -2,5 +2,5 @@ let color = 'black';
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color });
-    console.log('Default background color set to %cwhite', `color: ${color}`);
+    console.log('Default background color set to %cblack', `color: ${color}`);
 });
